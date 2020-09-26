a6eaad393aacf93bf3c74e2d6047382f7559931c
Albert John Patupat <Albert John Patupat@DESKTOP-I53QI0B>
2020-09-26 15:16:09 +0800

    FruitBasket

---
+++ codeitsuisse/routes/fruitbasket.py
@@ -0,0 +1,20 @@
+import logging
+import json
+
+from flask import request, jsonify;
+
+from codeitsuisse import app;
+
+logger = logging.getLogger(__name__)
+
+@app.route('/fruitbasket', methods=['POST'])
+def evaluateFruitBasket():
+    data = request.get_data()
+    logging.info("data sent for evaluation {}".format(data))
+
+    result = 1000
+    logging.info("My result :{}".format(result))
+    return str(result);
+
+
+
