42220320d3ff7c7cc798f50dfc5f3352e69c25ff
Albert John Patupat <Albert John Patupat@DESKTOP-I53QI0B>
2020-09-26 15:17:26 +0800

    F0

--- codeitsuisse/__init__.py
+++ codeitsuisse/__init__.py
@@ -5,5 +5,5 @@ import codeitsuisse.routes.salad_spree
 import codeitsuisse.routes.revisit_geometry
 import codeitsuisse.routes.clean_floor
 import codeitsuisse.routes.cluster
-
+import codeitsuisse.routes.fruitbasket
 
